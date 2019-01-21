55af01b98438a4af8f2c19cbdc46c3334d958f19
凌志文 <zhiwen.ling@lumiunited.com>
2019-01-21 10:34:28 +0800

    Feature:create project.

---
+++ src/main/java/com/example/administrator/myapplication/OpenGLRenderer.java
@@ -0,0 +1,63 @@
+package com.example.administrator.myapplication;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.annotation.TargetApi;
+import android.opengl.GLSurfaceView.Renderer;
+import android.opengl.GLU;
+import android.os.Build;
+
+@TargetApi(Build.VERSION_CODES.CUPCAKE)
+public class OpenGLRenderer implements Renderer {
+
+    private final IOpenGLDemo openGLDemo;
+
+    public OpenGLRenderer(IOpenGLDemo demo) {
+        openGLDemo = demo;
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        gl.glClearColor(0.0f, 0.0f, 0.0f, 0.5f);
+
+        gl.glShadeModel(GL10.GL_SMOOTH);
+
+        gl.glClearDepthf(1.0f);
+
+        gl.glEnable(GL10.GL_DEPTH_TEST);
+
+        gl.glDepthFunc(GL10.GL_LEQUAL);
+        gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_NICEST);
+
+        if (openGLDemo != null) {
+            openGLDemo.initLight(gl);
+
+            openGLDemo.initObject(gl);
+        }
+
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        if (openGLDemo != null) {
+            openGLDemo.DrawScene(gl);
+        }
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        // Sets the current view port to the new size.
+        gl.glViewport(0, 0, width, height);
+        // Select the projection matrix
+        gl.glMatrixMode(GL10.GL_PROJECTION);
+        // Reset the projection matrix
+        gl.glLoadIdentity();
+        // Calculate the aspect ratio of the window
+        GLU.gluPerspective(gl, 45.0f, (float) width / (float) height, 0.1f, 100.0f);
+        // Select the modelview matrix
+        gl.glMatrixMode(GL10.GL_MODELVIEW);
+        // Reset the modelview matrix
+        gl.glLoadIdentity();
+    }
+}
